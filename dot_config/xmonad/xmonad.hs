b046d3e156d50bc2d5c64dc2fe8aa4fdc00da902
Ian Melendez <ian.melendez.95@gmail.com>
2025-11-25 07:33:59 -0600

    update

--- dot_config/xmonad/xmonad.hs
+++ dot_config/xmonad/xmonad.hs
@@ -11,9 +11,12 @@ main =
         normalBorderColor = "#453A62"
       }
       `additionalKeysP` [ ("M-c", spawn "google-chrome-stable"),
-                          ("<XF86MonBrightnessUp>", spawn "brightnessctl set +200"),
-                          ("<XF86MonBrightnessDown>", spawn "brightnessctl set 200-"),
+                          ("<XF86MonBrightnessUp>", spawn $ "brightnessctl set +" ++ show bright_step),
+                          ("<XF86MonBrightnessDown>", spawn $ "brightnessctl set " ++ show bright_step ++ "-"),
                           ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 5%+"),
                           ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 5%-"),
                           ("<XF86AudioMute>", spawn "amixer -q sset Master toggle")
                         ]
+
+bright_step :: Int
+bright_step = 500
